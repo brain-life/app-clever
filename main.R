8cb4c2573041e52f21777dc1b285453e415b24ed
Soichi Hayashi <hayashis@iu.edu>
2018-06-19 17:38:11 -0400

    added mask

--- main.R
+++ main.R
@@ -1,11 +1,11 @@
 #!/usr/bin/env Rscript
 
-main <- function() {
-	args <- commandArgs(trailingOnly = TRUE)
-	bold_filename <- args[1]
-	print(bold_filename)
+args <- commandArgs(trailingOnly = TRUE)
+bold_filepath <- args[1]
+mask_filepath <- args[2]
+print(bold_filepath)
+print(mask_filepath)
 
-	## load bold.nii.gz, and convert it to 2d matrix and pass run clever!
+## load bold.nii.gz, and convert it to 2d matrix and pass run clever!
 
-	## convert output from clever to CSV and other data product
-}
+## convert output from clever to CSV and other data product
