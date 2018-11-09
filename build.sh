e2ffc739a780c00f5fd4b999875a8d2980194a4f
Soichi Hayashi <hayashis@iu.edu>
2018-11-09 21:40:19 +0000

    updated brainlife/clever container version

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
+set -e
+
 docker pull r-base:3.5.0
-docker build -t brainlife/clever . && \
-	docker tag brainlife/clever brainlife/clever:1.0 && \
-	docker push brainlife/clever
+docker build -t brainlife/clever . 
+docker tag brainlife/clever brainlife/clever:1.1
+docker push brainlife/clever
 
