a7c2b9663d8d38b9bf0b7b99c243e48fda49e503
damondpham <ddpham@iu.edu>
2020-03-25 16:18:17 -0400

    patches

--- utils.R
+++ utils.R
@@ -138,17 +138,16 @@ save_lev_imgs <- function(lev_imgs, out_dir='leverage_images'){
 	for(i in 1:length(lev_imgs$top_dir)){
 		t <- as.numeric(names(lev_imgs$top_dir))[i]
     save_mat_img(lev_imgs$mean[mid[1],,,i],
-								 paste0(out_dir, '/t', t, '_mean_1', s, '.png'))
+								 paste0(out_dir, '/t', t, '_mean_1.png'))
 		save_mat_img(lev_imgs$top[mid[1],,,i],
-								 paste0(out_dir, '/t', t, '_top_1', s, '.png'))
+								 paste0(out_dir, '/t', t, '_top_1.png'))
     save_mat_img(lev_imgs$mean[,mid[2],,i],
- 								 paste0(out_dir, '/t', t, '_mean_2', s, '.png'))
+ 								 paste0(out_dir, '/t', t, '_mean_2.png'))
  		save_mat_img(lev_imgs$top[,mid[2],,i],
- 								 paste0(out_dir, '/t', t, '_top_2', s, '.png'))
+ 								 paste0(out_dir, '/t', t, '_top_2.png'))
 		save_mat_img(lev_imgs$mean[,,mid[3],i],
-								 paste0(out_dir, '/t', t, '_mean_3', s, '.png'))
+								 paste0(out_dir, '/t', t, '_mean_3.png'))
 		save_mat_img(lev_imgs$top[,,mid[3],i],
-								 paste0(out_dir, '/t', t, '_top_3', s, '.png'))
-		}
+								 paste0(out_dir, '/t', t, '_top_3.png'))
 	}
 }
